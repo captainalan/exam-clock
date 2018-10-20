36acef491de718b72051fd24a01e15b59324186d
Alan Wong <captainalan@gmail.com>
2018-10-20 11:21:41 -0700

    Updated README and replaced course-specific stuff with something generic

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-(Description of this thing)
+# Exam Time and Instructions Display
+
+Classrooms at large universities often come equipped with projectors,
+screens, and other technology for instructional purposes.
+
+This App displays a large timer along with exam instructions and
+conditionally timed messages (e.g. Time's Up!). It is intended to put
+on-screen during exams.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-Below you will find some information on how to perform common tasks.<br>
-You can find the most recent version of this guide [here](https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md).
-
-## Table of Contents
-
-(Add stuff soon)
 
--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 const exam_info = {
-  course:"LIN 001",
+  course:"Underwater Basket Weaving",
   name: "Midterm 1",
   start_time:'19 Oct 2018 21:10:00 UTC',
   end_time:'19 Oct 2018 22:00:00 UTC',
+  instructions: "Remember to write your name and student ID on your scantron. Bubble in your exam version number as well! Please take your paper exam sheet with you when you finish.",
 }
 
 class App extends Component {
@@ -13,10 +14,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{exam_info.course}: {exam_info.name} </h1>
-        {/* Tick tock! */}
+        {/* Convert Date string in JSON object to Date object here*/}
         <ExamClock 
           start_time = {new Date(exam_info.start_time)}
           end_time = {new Date(exam_info.end_time)}
+          instructions = {exam_info.instructions}
         />
       </div>
     );
@@ -47,36 +49,31 @@ class ExamClock extends Component {
   }
 
   render() {
-    // For displaying times
+    // For styling conditionally rendered announcements
     let unurgent = {color: 'grey'};
     let ongoing = {color: 'lime'}
     let urgent = {color: 'red'};
 
     return(
-      <div>
-        {(this.state.date < this.props.start_time) ? 
+      <div> {/* Conditionally rendered announcement appear above large timer */}
+        { /* Exam hasn't yet begun */
+          (this.state.date < this.props.start_time) ? 
           <span style={unurgent}>Hold up! The exam hasn't started yet.</span>
+          /* Exam has started! */
           : (this.state.date < this.props.end_time) ? 
             <span style={ongoing}>Exam in progress...</span>
+            /* Exam has finished! */
             : <span style={urgent}>Time's up! Please turn in your scantron.</span>}
 
         <h2 className="timer">{this.state.date.toLocaleTimeString()}</h2>
         <div className="announcement">
-
           <h3>Start time: {this.props.start_time.toLocaleTimeString()}</h3>
           <h3>End time: {this.props.end_time.toLocaleTimeString()}</h3>
-
-          <p>Remember to write your name and student ID on your scantron. Bubble in your <b>version number</b> as well!
-            Please take your paper exam sheet with you when you finish.</p>
-
-          <p><b>Turning in stuff at the front table:</b><br /> Version A on the LEFT, Version B on the RIGHT</p>
-          <img src="versions.png" alt="Versions A and B note"></img>
-
+          <p>{this.props.instructions}</p>
         </div>
       </div>
     );
   }
 }
-
 
 export default App;
